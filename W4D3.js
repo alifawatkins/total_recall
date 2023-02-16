7df4cbc5f5d9227beffb59ac01a6b7163a319f5a
littlelionstudios <flavorformulas@gmail.com>
2023-02-16 15:30:48 -0500

    Get Even answered

--- W4D3.js
+++ W4D3.js
@@ -5,7 +5,11 @@ for (let i = 1; i <= 20; i++) {
     console.log(i);
 }
 
-console.log("Hello?");
+for (let i = 0; i <= 200; i++) {
+    if (i % 2 === 0) {
+        console.log(i); 
+    } 
+}
 ////////////////////////////////
 // Get Even
 ////////////////////////////////
