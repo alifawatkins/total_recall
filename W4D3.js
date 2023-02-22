21611958fc91f5b88443c6c7be19b7301a721c1f
littlelionstudios <flavorformulas@gmail.com>
2023-02-21 21:09:32 -0500

    Where is Waldo answered

--- W4D3.js
+++ W4D3.js
@@ -82,7 +82,18 @@ console.log(favMovies.indexOf('Fast and Furious'))
 ////////////////////////////////
 // Where is Waldo
 ////////////////////////////////
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
 
+whereIsWaldo.splice(1,1);
+console.log(whereIsWaldo);
+
+whereIsWaldo[1][2] = "No One";
+console.log(whereIsWaldo);
+
+const waldo = whereIsWaldo[2][1][1];
+console.log(waldo);
 
 ////////////////////////////////
 //  Excited Kitten
